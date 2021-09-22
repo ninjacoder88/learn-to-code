170ae28f91b94bb8d3df74cb1bb843bcca7261b4
Tommy Harris <tomharris@idtdna.com>
2021-09-22 12:21:06 -0500

    added weighted graph to maker

--- src/05-Graphs/Graphs/Helpers/GraphMaker.cs
+++ src/05-Graphs/Graphs/Helpers/GraphMaker.cs
@@ -29,5 +29,37 @@ namespace Graphs
 
             return graph;
         }
+
+        public WeightedGraph MakeWeighted()
+        {
+            WeightedVertex zero = new WeightedVertex("0");
+            WeightedVertex one = new WeightedVertex("1");
+            WeightedVertex two = new WeightedVertex("2");
+            WeightedVertex three = new WeightedVertex("3");
+            WeightedVertex four = new WeightedVertex("4");
+            WeightedVertex five = new WeightedVertex("5");
+            WeightedVertex six = new WeightedVertex("6");
+
+            WeightedGraph g = new WeightedGraph();
+            g.AddVertex(zero);
+            g.AddVertex(one);
+            g.AddVertex(two);
+            g.AddVertex(three);
+            g.AddVertex(four);
+            g.AddVertex(five);
+            g.AddVertex(six);
+
+            g.AddEdge(zero, one, 2, 2);
+            g.AddEdge(zero, two, 6, 6);
+            g.AddEdge(one, three, 5, 5);
+            g.AddEdge(two, three, 8, 8);
+            g.AddEdge(three, four, 10, 10);
+            g.AddEdge(three, five, 15, 15);
+            g.AddEdge(four, five, 6, 6);
+            g.AddEdge(four, six, 2, 2);
+            g.AddEdge(five, six, 6, 6);
+
+            return g;
+        }
     }
 }

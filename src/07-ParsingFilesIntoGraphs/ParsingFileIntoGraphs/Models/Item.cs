41d0911cb4b7c20f61b9e462799884d1421a6be9
Tommy Harris <tomharris@idtdna.com>
2021-09-22 14:47:57 -0500

    removed unused property

--- src/07-ParsingFilesIntoGraphs/ParsingFileIntoGraphs/Models/Item.cs
+++ src/07-ParsingFilesIntoGraphs/ParsingFileIntoGraphs/Models/Item.cs
@@ -5,6 +5,5 @@ namespace ParsingFilesIntoGraphs
         public string StartCity {get;set;}
         public string EndCity {get;set;}
         public decimal Distance {get;set;}
-        public int TimeInMinutes {get;set;}
     }
 }

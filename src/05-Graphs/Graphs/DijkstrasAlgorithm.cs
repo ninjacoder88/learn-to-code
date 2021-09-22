83dbc4c7f7d1c558a1de50af49326f3eefbc0206
Tommy Harris <tomharris@idtdna.com>
2021-09-22 12:23:12 -0500

    finished algorithm

--- src/05-Graphs/Graphs/DijkstrasAlgorithm.cs
+++ src/05-Graphs/Graphs/DijkstrasAlgorithm.cs
@@ -1,35 +1,115 @@
+using System;
 using System.Collections.Generic;
 
 namespace Graphs
 {
     public class DijkstrasAlgorithm
     {
-        public void Traverse(Vertex start, string destination)
+        public void FindWeighted(WeightedGraph g, WeightedVertex source)
         {
-            Queue<Vertex> queue = new Queue<Vertex>();
-            queue.Enqueue(start);
+            List<WeightedVertex> unvisited = new List<WeightedVertex>();
+            Dictionary<WeightedVertex, decimal> dist = new Dictionary<WeightedVertex, decimal>();
+            Queue<WeightedVertex> path = new Queue<WeightedVertex>();
 
-            while(queue.Count > 0)
+            foreach(var vertex in g.Verticies)
             {
-                var current = queue.Dequeue();
+                if(vertex == source)
+                    dist.Add(vertex, 0);
+                else
+                    dist.Add(vertex, decimal.MaxValue);
+                
+                unvisited.Add(vertex);
+            }
 
-                int minimumDistance = int.MaxValue;
-                Vertex next = null;
+            unvisited.Remove(source);
+            path.Enqueue(source);
 
-                foreach(var edge in start.Edges)
+            while(unvisited.Count > 0)
+            {
+                foreach(var vertex in path)
                 {
-                    //check if vertex has been visited, if not queue and check distance
-
-                    //find the closest vertex
-                    int distance = 1;
-                    if(distance < minimumDistance)
+                    foreach(var edge in vertex.Edges)
                     {
-                        minimumDistance = distance;
-                        next = edge.Destination;
+                        var d = dist[vertex] + edge.Distance;
+                        if(d < dist[edge.Destination])
+                            dist[edge.Destination] = d;
                     }
                 }
 
-                //repeat with next
+                decimal minimumDistance = decimal.MaxValue;
+                WeightedVertex closestVertex = null;
+                foreach(var entry in dist)
+                {
+                    if(!unvisited.Contains(entry.Key))
+                        continue;
+                    if(entry.Value < minimumDistance)
+                    {
+                        minimumDistance = entry.Value;
+                        closestVertex = entry.Key;
+                    }
+                }
+
+                unvisited.Remove(closestVertex);
+                path.Enqueue(closestVertex);
+            }
+
+            foreach(var entry in dist)
+            {
+                Console.WriteLine($"{entry.Key.Name} -> {entry.Value}");
+            }
+        }
+
+        public void FindSimple(Graph g, Vertex source)
+        {
+            List<Vertex> unvisited = new List<Vertex>();
+            Dictionary<Vertex, decimal> dist = new Dictionary<Vertex, decimal>();
+            Queue<Vertex> path = new Queue<Vertex>();
+
+            foreach(var vertex in g.Verticies)
+            {
+                if(vertex == source)
+                    dist.Add(vertex, 0);
+                else
+                    dist.Add(vertex, decimal.MaxValue);
+                
+                unvisited.Add(vertex);
+            }
+
+            unvisited.Remove(source);
+            path.Enqueue(source);
+
+            while(unvisited.Count > 0)
+            {
+                foreach(var vertex in path)
+                {
+                    foreach(var edge in vertex.Edges)
+                    {
+                        var d = dist[vertex] + 1;
+                        if(d < dist[edge.Destination])
+                            dist[edge.Destination] = d;
+                    }
+                }
+
+                decimal minimumDistance = decimal.MaxValue;
+                Vertex closestVertex = null;
+                foreach(var entry in dist)
+                {
+                    if(!unvisited.Contains(entry.Key))
+                        continue;
+                    if(entry.Value < minimumDistance)
+                    {
+                        minimumDistance = entry.Value;
+                        closestVertex = entry.Key;
+                    }
+                }
+
+                unvisited.Remove(closestVertex);
+                path.Enqueue(closestVertex);
+            }
+
+            foreach(var entry in dist)
+            {
+                Console.WriteLine($"{entry.Key.Name} -> {entry.Value}");
             }
         }
     }

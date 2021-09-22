3c61aec73e0fed91ec293fa7b1352832493815a3
Tommy Harris <tomharris@idtdna.com>
2021-09-22 12:20:50 -0500

    added weighted graph

---
+++ src/05-Graphs/Graphs/Models/Weighted/WeightedEdge.cs
@@ -0,0 +1,26 @@
+namespace Graphs
+{
+    public class WeightedEdge
+    {
+        public WeightedEdge(WeightedVertex source, WeightedVertex destination, decimal distance, int time)
+        {
+            Source = source;
+            Destination = destination;
+            Distance = distance;
+            Time = time;
+        }
+
+        public WeightedVertex Source {get;}
+
+        public WeightedVertex Destination {get;}
+
+        public decimal Distance {get;}
+
+        public int Time {get;}
+
+        public override string ToString()
+        {
+            return $"{Source.Name} => {Destination.Name}";
+        }
+    }
+}
